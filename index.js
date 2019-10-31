07796ba97fa4e1307187071fa0c5f79b90eae77e
Alyona <itsbronza@yahoo.com>
2019-10-31 10:41:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ return "Happy holidays!";
 function happyHolidaysTo(name){
   return "Happy holidays, ${name}!";
 }
+
+function happyHolidayTo(){
+  return `Happy ${holiday}, ${name}!`;
+}
+
+function holidayCountdown(){
+  return `It's ${days} days until ${holiday}!`;
+}
